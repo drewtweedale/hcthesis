73d6db829e0c7bee053b0a50d84b1c94737840f1
drewtweedale <drewtweedale@gmail.com>
2025-04-15 14:48:18 -0700

    Added updated figures for magnetic fields.

--- thesis code/combined_field.m
+++ thesis code/combined_field.m
@@ -3,6 +3,6 @@ function B = combined_field(r, B0, RN, eta, g10, g11, h11, g20, g21, h21, g22, h
     B_dipole = dipole_field(r, RN, g10, g11, h11);
     % B_dipole = dipole_field(r, RN, B0);
     B_quadrupole = quadrupole_field(r, RN, g20, g21, h21, g22, h22);
-    % B_quadrupole = quadrupole_field(r, RN, eta);
-    B = B_quadrupole + B_dipole;
+    % B_quadrupole = quadrupole_field(r, RN);
+    B = B_dipole + B_quadrupole;
 end
